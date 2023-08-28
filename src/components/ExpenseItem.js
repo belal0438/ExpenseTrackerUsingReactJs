99b0c6f2747f24b638c460e33df31db415b837f0
Belal <mohammadvns121@gmail.com>
2023-08-28 21:31:05 +0530

    start with react

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurence</h2>
+        <div>$294.67</div>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseItem;
