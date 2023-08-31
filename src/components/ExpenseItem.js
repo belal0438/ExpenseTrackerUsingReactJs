c97e8b3ff894d7ca40d130ef70c936f12d31e244
Belal <mohammadvns121@gmail.com>
2023-08-31 23:16:13 +0530

    Making Components Smaller and Readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,12 @@
 import React from "react";
 import "./ExpenseItem.css";
-
-//props we name name anything instaed of props, it work on key value pair
-//Ex:-
-//  title={expenses[1].title}
-//  amount={expenses[1].amount}
-//  date={expenses[1].date}
-
-// where tilte, amount, date are key of respective value
-// props are just like a object
-// props allows you to make your component reusable
-// props allows you to passed data from another component to these component
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
   return (
     <>
       <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
